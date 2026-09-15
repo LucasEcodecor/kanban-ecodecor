5007ba50be7a84b6fc1a8c8ed03b1cc3f3f829d9
LucasEcodecor <luu.oliveira2105@gmail.com>
2026-09-15 14:57:38 -0300

    Update kanban_equipe.py

--- kanban_equipe.py
+++ kanban_equipe.py
@@ -311,7 +311,7 @@ def obter_capacidade(cliente, tam):
     if tam == "60x40":
         return 24
     if tam == "30x40":
-        return 50
+        return 47
     if tam in ("20x50", "50x20"):
         return 50
     if tam == "55x35":
